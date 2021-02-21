c4c01dd37f3b70cf57fe06fd2be28c14acff414c
Ruairi O Brien <ruairitobrien@gmail.com>
2021-02-21 15:34:36 +0000

    Use firebase

--- server.js
+++ server.js
@@ -17,18 +17,41 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-app.all(
-  "*",
-  createRequestHandler({
-    getLoadContext() {
-      // Whatever you return here will be passed as `context` to your loaders
-      // and actions.
-    }
-  })
-);
+if (require.main === module) {
+  app.use(express.static("public"));
+  app.all(
+    "*",
+    createRequestHandler({
+      // Uncomment the following line if you don't want sessions. This will
+      // disable the warning message when no session middleware is present.
+      enableSessions: false,
+      getLoadContext(req) {
+        // Whatever you return here will be passed as `context` to your loaders
+        // and actions.
+        return { req }
+      },
+    })
+  );
 
-let port = process.env.PORT || 3000;
+  let port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-  console.log(`Express server started on http://localhost:${port}`);
-});
+  app.listen(port, () => {
+    console.log(`Express server started on http://localhost:${port}`);
+  });
+} else {
+  app.all(
+    "*",
+    createRequestHandler({
+      // Uncomment the following line if you don't want sessions. This will
+      // disable the warning message when no session middleware is present.
+      enableSessions: false,
+      getLoadContext(req) {
+        // Whatever you return here will be passed as `context` to your loaders
+        // and actions.
+        return { req }
+      },
+    })
+  );
+}
+
+module.exports = app;
